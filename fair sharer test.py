444f306c1b40ffa1e80e885222296355ac8e8111
Ersan Kahraman <115080173+ErsanK61@users.noreply.github.com>
2023-02-14 11:44:59 +0100

    Update fair sharer test.py

--- fair sharer test.py
+++ fair sharer test.py
@@ -1,9 +1,6 @@
 import numpy as np
 from fairsharer import fair_sharer
 
-
 def test_fair_sharer():
-   
     assert np.allclose(fair_sharer([0, 1000, 800, 0], 1), [100, 800, 900, 0])
-
-   
+    assert np.allclose(fair_sharer([0, 1000, 800, 0], 2) ,[100, 890, 720, 90])
